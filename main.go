5b40c4e37052cb863668874c216186ef47acba5f
Konstantin Antonov <tino.ant.s@gmail.com>
2020-06-08 21:30:00 +0300

    updated algorithm

--- main.go
+++ main.go
@@ -4,39 +4,78 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
+	"sort"
 	"strings"
 )
 
 const (
 	curVisual  string = "├───"
+	lastVisual string = "└───"
 	prevVisual string = "│\t"
 )
 
-func getFilePrefix(path string) string {
+func getFilePrefix(path string, isLast bool, depth int) string {
 	var prefix string
-	n := strings.Count(path, "/")
-	if n > 0 {
-		prefix = strings.Repeat(prevVisual, n-1) + curVisual
+	vis := curVisual
+	if isLast {
+		vis = lastVisual
 	}
+	prefix = strings.Repeat(prevVisual, depth-1) + vis
 	return prefix
 
 }
 
-func step(path string, info os.FileInfo, err error) error {
-	fname := info.Name()
-	prefix := getFilePrefix(path)
-	fmt.Println(prefix, fname)
+func getOnlyDirs(objects []os.FileInfo) (dirs []os.FileInfo) {
+	for _, info := range objects {
+		if info.IsDir() {
+			dirs = append(dirs, info)
+		}
+	}
+	return
+}
+
+func sizeOrEmpty(size int64) (sizeStr string) {
+	if size == 0 {
+		sizeStr = "(empty)"
+	} else {
+		sizeStr = fmt.Sprintf("(%vb)", size)
+	}
+	return
+}
+
+func describeDir(out io.Writer, path string, withFiles bool, curDepth int) error {
+	curObject, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	dirContent, err := curObject.Readdir(0)
+	if err != nil {
+		return err
+	}
+	sort.Slice(dirContent, func(i, j int) bool { return dirContent[i].Name() < dirContent[j].Name() })
+	if !withFiles {
+		dirContent = getOnlyDirs(dirContent)
+	}
+	for i, info := range dirContent {
+		isLast := i == len(dirContent)-1
+		if info.IsDir() {
+			fmt.Fprintf(out, "%v%v\n", getFilePrefix(path, isLast, curDepth), info.Name())
+			err := describeDir(out, path+"/"+info.Name(), withFiles, curDepth+1)
+			if err != nil {
+				return err
+			}
+		} else if withFiles {
+			fmt.Fprintf(out, "%v%v %v\n", getFilePrefix(path, isLast, curDepth), info.Name(), sizeOrEmpty(info.Size()))
+		}
+	}
 	return nil
 }
 
 func dirTree(out io.Writer, path string, withFiles bool) error {
-	var res string
-	err := filepath.Walk(path, step)
+	err := describeDir(out, path, withFiles, 1)
 	if err != nil {
 		return err
 	}
-	fmt.Fprint(out, res)
 	return nil
 }
 
